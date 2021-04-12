89a99990c816676abb3bd46b3116fad295e570c2
jakobfipke <60725504+jakobfipke@users.noreply.github.com>
2021-04-12 15:47:35 -0700

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,7 +9,6 @@ import ProjectPage from './pages/ProjectPage';
 import GoalsPage from './pages/GoalsPage';
 import JoinTeamPage from './pages/JoinTeamPage';
 import NavBar from './NavBar';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/umd/popper.min.js';
@@ -22,38 +21,26 @@ import {
 } from 'react-router-dom';
 
 function App() {
-//   if (localStorage.getItem("userID") != undefined && localStorage.getItem("authToken") != undefined) {
-//     return (
-//       <Router>
-//         <NavBar />
-//         <div className="container">
-//           <Switch>
-//             <Route path="/" component={DashboardPage} exact />
-//             <Route path="/outlines/:id" component={OutlinesPage} exact />
-//             <Route path="/outlines/outline/:id" component={OutlinePage} exact />
-//             <Route path="/outlines/outline/:id/project/" component={ProjectPage} exact />
-//             <Route path="/outlines/outline/:id/project/join" component={JoinTeamPage} exact />
-//             <Route path="/outlines/outline/:id/project/:projectId/goals" component={GoalsPage} exact />
-//             <Route component={NotFoundPage} />
-//           </Switch>
-//         </div>
-//       </Router>
-//     );
-//   } else {
-    return (
-      <Router>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route path="/" component={LandingPage} exact />
-            <Route path="/login" component={LoginPage} exact />
-            <Route path="/register" component={RegisterPage} exact />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </Router>
-    );
-//   }
+  return (
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          {/* <Route path="/" component={HomePage} exact /> */}
+          <Route path="/" component={LandingPage} exact />
+          <Route path="/login" component={LoginPage} exact />
+          <Route path="/register" component={RegisterPage} exact />
+          <Route path="/dashboard/" component={DashboardPage} exact />
+          <Route path="/outlines/:id" component={OutlinesPage} exact />
+          <Route path="/outlines/outline/:id" component={OutlinePage} exact />
+          <Route path="/outlines/outline/:id/project/" component={ProjectPage} exact />
+          <Route path="/outlines/outline/:id/project/join" component={JoinTeamPage} exact />
+          <Route path="/outlines/outline/:id/project/:projectId/goals" component={GoalsPage} exact />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
